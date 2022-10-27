6e96013fa92c1e8add704fb641f39d5928c725d6
TazK7 <khantazz07@gmail.com>
2022-10-27 10:17:12 -0400

    Coding function for Previous Button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 let main_container = document.querySelector(`main-container`);
 
 let i = 1;
+
+function prev() {
+    if (i === 1) {
+        document.getElementById('prev').disabled = true;
+    }
+}
