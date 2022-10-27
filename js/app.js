31af7ca19ad4b7952e3c0ab870c3d9b386423ae8
TazK7 <khantazz07@gmail.com>
2022-10-27 10:36:05 -0400

    Finishing Touches (Couldn't solve Project)
    I understood what I had to do w/ HTML & CSS but I couldn't manage to code the JS functions for buttons. Will come to office hours for help soon.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
+const { set } = require("ig");
+
 let main_container = document.querySelector(`main-container`);
 
 let i = 1;
 
 function prev() {
     if (i === 1) {
-        document.getElementById('prev').disabled = true;
+        document.getElementById(`prev`).disabled = true;
+
+        document.getElementById(`next`).disabled = false;
+    } else {
+        i--;
+        return setFirst();
     }
 }
